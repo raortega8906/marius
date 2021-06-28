60a91cc49f0c74e9148dc3f2d1d8306d51713cd0
Rafael <raortega8906@gmail.com>
2021-06-28 02:42:46 +0200

    nombre del titulo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Marius</title>
+    <title>Marius | Empresa de Reformas</title>
     <link rel="stylesheet" href="/bluesky/css/bootstrap.min.css">
     <link rel="stylesheet" href="/bluesky/css/style.css">
     <link rel="stylesheet" href="/bluesky/css/fontawesome.min.css">
